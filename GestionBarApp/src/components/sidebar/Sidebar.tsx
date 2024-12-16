16077f56fac6b3a68ba8b12586c2dd63282d698d
JeronimoLam <jerolambre@gmail.com>
2024-12-16 12:54:34 -0300

    Sidebar mas facha

--- GestionBarApp/src/components/sidebar/Sidebar.tsx
+++ GestionBarApp/src/components/sidebar/Sidebar.tsx
@@ -1,9 +1,12 @@
 import './Sidebar.css';
 import { Link } from 'react-router-dom';
 import { BarChart2, Package, DollarSign, Coffee, Menu } from 'react-feather';
+import { PiForkKnife  } from 'react-icons/pi'; // Import the icon
+
 
 const Sidebar = ({ isExpanded, toggleSidebar }: { isExpanded: boolean; toggleSidebar: () => void }) => {
   const menuItems = [
+    { icon: PiForkKnife , label: 'Mesas', href: '/mesas' },
     { icon: BarChart2, label: 'Datos de mi negocio', href: '/datos' },
     { icon: Package, label: 'Stock', href: '/stock' },
     { icon: DollarSign, label: 'Balance', href: '/balance' },
